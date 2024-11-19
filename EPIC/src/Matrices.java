d7a97073d0abb591d8bb9774dc57fb9c2036a1a8
sarah <sarahmcdon2005@gmail.com>
2024-11-19 18:43:49 +0000

    working matrix multiplication method

--- EPIC/src/Matrices.java
+++ EPIC/src/Matrices.java
@@ -16,13 +16,24 @@ public class Matrices {
 
     public int[][] matrixSubtraction(int[][] a, int[][] b) { // method to add two matrices
 
-        // creating a new 2d array to store the result and setting the
-        // dimensions to the same as a and b
         int[][] c = new int[a.length][a[0].length];
 
         for (int i = 0; i < a.length; i++) { // iterating through each row
             for (int j = 0; j < a[0].length; j++) { // iterating through each column in the row
-                c[i][j] = a[i][j] - b[i][j]; // adding the elements a and b together and storing the result in c
+                c[i][j] = a[i][j] - b[i][j]; // subtracting the elements a and b together and storing the result in c
+            }
+        }
+        return c;
+    }
+
+    public int[][] matrixMultiplication(int[][] a, int[][] b) {
+        int[][] c = new int[a.length][b[0].length];
+
+        for(int i = 0; i < a.length; i++) { // iterating through rows
+            for(int j = 0; j < b[0].length; j++) { // iterating through columns
+                for(int k = 0; k < b.length; k++) { // iterating through shared dimension
+                    c[i][j] += a[i][k] * b[k][j];
+                }
             }
         }
         return c;
--- EPIC/src/MatricesTest.java
+++ EPIC/src/MatricesTest.java
@@ -1,14 +1,31 @@
 public class MatricesTest {
     public static void main(String[] args) {
-        int[][] identityMatrix = {{1, 0}, {0, 1}};
-        int[][] a = {{2, 2}, {2, 2}};
+        int[][] a = {{1, 0}, {0, 1}};
+        int[][] b = {{2, 2}, {2, 2}};
         Matrices matrices = new Matrices();
 
-        int[][] c = matrices.matrixSubtraction(identityMatrix, a);
+        int[][] addition = matrices.matrixAddition(a, b);
+        for(int i = 0; i < addition.length; i++) {
+            for(int j = 0; j < addition[i].length; j++) {
+                System.out.print(addition[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
 
-        for(int i = 0; i < c.length; i++) {
-            for(int j = 0; j < c[i].length; j++) {
-                System.out.print(c[i][j]);
+        int[][] subtraction = matrices.matrixSubtraction(a, b);
+        for(int i = 0; i < subtraction.length; i++) {
+            for(int j = 0; j < subtraction[i].length; j++) {
+                System.out.print(subtraction[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        int[][] multiplication = matrices.matrixMultiplication(a, b);
+        for(int i = 0; i < multiplication.length; i++) {
+            for(int j = 0; j < multiplication[i].length; j++) {
+                System.out.print(multiplication[i][j]);
                 System.out.print(" ");
             }
             System.out.println();
