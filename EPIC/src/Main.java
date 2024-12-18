7c60caa5be1d396c5ae6c7383e0d1e42db810954
Cian McNamara <cian@telecomeireann.ie>
2024-12-18 17:06:57 +0000

    scientific calculator menu made

--- EPIC/src/Main.java
+++ EPIC/src/Main.java
@@ -2,11 +2,99 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        displayMainMenu();
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Insert equation: ");
-        String input = sc.nextLine();
-        //System.out.println("Hello world!");
-        LatexInterpreter interpreter = new LatexInterpreter(input);
-        System.out.println("Answer: " + interpreter);
+
+        while (true) {
+            try {
+                int input = sc.nextInt();
+
+                switch (input) {
+                    case 1:
+                        displayLatexMenu();
+                        break;
+                    case 2:
+                        displayMatrixMenu();
+                        break;
+                    case 3:
+                        displayStatisticsMenu();
+                        break;
+                    case 4:
+                        displayUnitConversionMenu();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        System.out.println("Invalid input. Try again.");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Try again.");
+                sc.nextLine();
+            }
+        }
+    }
+
+    public static void displayMainMenu() {
+        System.out.printf(
+                """
+                        ███╗   ███╗ █████╗ ██╗███╗   ██╗    ███╗   ███╗███████╗███╗   ██╗██╗   ██╗
+                        ████╗ ████║██╔══██╗██║████╗  ██║    ████╗ ████║██╔════╝████╗  ██║██║   ██║
+                        ██╔████╔██║███████║██║██╔██╗ ██║    ██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║
+                        ██║╚██╔╝██║██╔══██║██║██║╚██╗██║    ██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║
+                        ██║ ╚═╝ ██║██║  ██║██║██║ ╚████║    ██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝
+                        ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝\s
+                        """
+        );
+        System.out.printf("\n" + """
+                ╔════════════════════════════════╗
+                ║ 1. LaTeX Scientific Calculator ║
+                ║ 2. Matrix Calculator           ║
+                ║ 3. Statistics Calculator       ║
+                ║ 4. Unit Conversions            ║
+                ║ 5. Exit                        ║
+                ╚════════════════════════════════╝
+                """);
+        System.out.println("\nSelect an option from the menu.");
+        System.out.printf("\nChoice: ");
+    }
+
+    public static void displayLatexMenu() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("LaTex Scientific Calculator\nEnter :q to quit.");
+
+        try {
+            while (true) {
+                String input = sc.nextLine();
+
+                if (input.equals(":q")) {
+                    System.out.println("Exiting...");
+                    displayMainMenu();
+                    return;
+                } else {
+                    System.out.println(input + " = " + new LatexInterpreter(input).sum);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void displayMatrixMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Define row length.");
+        int rowLength = sc.nextInt();
+        System.out.println("Define column length.");
+        int columnLength = sc.nextInt();
+    }
+
+    public static void displayStatisticsMenu() {
+
+    }
+
+    public static void displayUnitConversionMenu() {
+
     }
 }
