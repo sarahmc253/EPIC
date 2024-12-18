7b62099975b7c3ea25bde1c40c6000607e3baf18
Sarah <sarahmcdon2005@gmail.com>
2024-12-18 18:40:09 +0000

    main with addition method done

--- EPIC/src/Matrices.java
+++ EPIC/src/Matrices.java
@@ -5,6 +5,50 @@ public class Matrices {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+
+        System.out.println("1. Addition \n2. Subtraction \n3. Multiplication \n4. Transpose \n5. Determinant" +
+                "\n6. Inverse (2x2) \n7. Matrix Linear Equation \n8. Dot Product \n9. Find Eigenvalues");
+        System.out.print("Enter value of your desired method: ");
+
+        int value = input.nextInt();
+
+        System.out.println();
+
+        switch (value) {
+            case 1:
+                System.out.printf("Enter matrix a:%n");
+                double[][] a = inputMatrix();
+
+                System.out.printf("Enter matrix b:%n");
+                double[][] b = inputMatrix();
+
+                double[][] answer = matrixAddition(a, b);
+
+                System.out.print(toString(answer));
+        }
+
+    }
+
+    public static double[][] inputMatrix() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = input.nextInt();
+        System.out.printf("%nEnter the number of columns: ");
+        int columns = input.nextInt();
+
+        double[][] matrix = new double[rows][columns];
+
+        System.out.println("Enter the elements of the matrix row by row:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.printf("Element [%d][%d]: ", i, j);
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        return matrix;
     }
 
     public static double[][] matrixAddition(double[][] a, double[][] b) { // method to add two matrices
@@ -308,4 +352,6 @@ public class Matrices {
             throw new IllegalArgumentException("Matrices must have the same dimensions.");
         }
     }
+
+
 }
